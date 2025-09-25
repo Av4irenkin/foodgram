817ed4d66c2875d0983422cba23dfba1466bc8db
Av4irenkin <Av4rnk@gmail.com>
2025-09-25 15:17:24 +0500

    current progress++

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,9 +2,21 @@ from rest_framework import serializers
 from django.contrib.auth import authenticate
 from food.models import (User, Follow, Ingredient, Recipe,
                          Tag, RecipeIngredient, Favorite, ShoppingCartItem)
+from django.core.files.base import ContentFile
+from rest_framework import serializers
+import base64
 
 
-class DetailsUsersSerializer(serializers.ModelSerializer):
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+        return super().to_internal_value(data)
+
+
+class UserSerializer(serializers.ModelSerializer):
     is_subscribed = serializers.SerializerMethodField(read_only=True)
     avatar = Base64ImageField(read_only=True)
 
